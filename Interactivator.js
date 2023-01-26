244f5130a8646af4cc8fdc645034b1e043d776d9
Kaleem Mohammad <62307224+kaleem99@users.noreply.github.com>
2023-01-26 17:22:32 +0200

    Update Interactivator.js

--- Interactivator.js
+++ Interactivator.js
@@ -1,9 +1,17 @@
 let quizIndex = 0;
 let optionChosen = "";
 let videoId = document.querySelector("iframe").src;
-let isQuizComplete = ""
+let isQuizComplete = "";
+let countButtons = 0;
 videoId = videoId.slice(0, videoId.indexOf("?"));
 videoId = videoId.slice(videoId.lastIndexOf("/") + 1, videoId.length);
+let countVideoButton =
+  document.querySelectorAll("#section")[quizIndex].children;
+for (let i = 0; i < countVideoButton.length; i++) {
+  if (countVideoButton[i].className.includes("buttonQuiz")) {
+    countButtons++;
+  }
+}
 function quizComplete(arr) {
   isQuizComplete = arr;
 }
@@ -11,13 +19,16 @@ function changeAnswer(option) {
   optionChosen = option;
   let elementOptions = document.getElementsByClassName("buttonQuiz option");
   let num = option[option.length - 1];
+  if (quizIndex > 0) {
+    num = parseInt(num) + countButtons;
+  }
   for (let i = 0; i < elementOptions.length; i++) {
     elementOptions[i].style.backgroundColor = "#009cde";
   }
-  elementOptions[num - 1].style.backgroundColor = "035dae";
+  elementOptions[num - 1].style.backgroundColor = "#035dae";
 }
 function checkAnswer() {
-  let inputs = document.querySelectorAll(".buttonQuiz");
+  let inputs = document.querySelectorAll("#section")[quizIndex].children;
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].className.includes("correct")) {
       if (inputs[i].id === optionChosen) {
