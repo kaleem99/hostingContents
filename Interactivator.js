75c007e6b0cd654867035e5f062b3b8dd5c067b1
Kaleem Mohammad <62307224+kaleem99@users.noreply.github.com>
2023-01-30 22:39:16 +0200

    Update Interactivator.js

--- Interactivator.js
+++ Interactivator.js
@@ -3,32 +3,45 @@ let optionChosen = "";
 let videoId = document.querySelector("iframe").src;
 let isQuizComplete = "";
 let countButtons = 0;
+let ArrayOfQuestions = [];
 videoId = videoId.slice(0, videoId.indexOf("?"));
 videoId = videoId.slice(videoId.lastIndexOf("/") + 1, videoId.length);
-let countVideoButton =
-  document.querySelectorAll("#section")[quizIndex].children;
-for (let i = 0; i < countVideoButton.length; i++) {
-  if (countVideoButton[i].className.includes("buttonQuiz")) {
-    countButtons++;
+
+function quizComplete(arr, QuestionsArr) {
+  isQuizComplete = arr;
+  ArrayOfQuestions = QuestionsArr;
+}
+function generateQuestionsAndOptions() {
+  let question = document.getElementById("Question");
+  question.innerHTML = ArrayOfQuestions[quizIndex].Question;
+  let QuestionsfromArr = Object.keys(ArrayOfQuestions[quizIndex]).length;
+  let divInputs = document.getElementById("InputSection");
+  for (let i = 0; i < QuestionsfromArr - 2; i++) {
+    divInputs.innerHTML += ` <button class="buttonQuiz option ${
+      ArrayOfQuestions[quizIndex]["Option" + (i + 1)] ===
+      ArrayOfQuestions[quizIndex].correct
+        ? "correct"
+        : ""
+    }" id='Option ${i + 1}' onclick="changeAnswer(\`Option ${
+      i + 1
+    }\`)" tabindex="0"> ${
+      ArrayOfQuestions[quizIndex]["Option" + (i + 1)]
+    }</button><br /><br />`;
   }
 }
-function quizComplete(arr) {
-  isQuizComplete = arr;
-}
+
 function changeAnswer(option) {
+  console.log(option);
   optionChosen = option;
   let elementOptions = document.getElementsByClassName("buttonQuiz option");
   let num = option[option.length - 1];
-  if (quizIndex > 0) {
-    num = parseInt(num) + countButtons;
-  }
   for (let i = 0; i < elementOptions.length; i++) {
     elementOptions[i].style.backgroundColor = "#009cde";
   }
   elementOptions[num - 1].style.backgroundColor = "#035dae";
 }
 function checkAnswer() {
-  let inputs = document.querySelectorAll("#section")[quizIndex].children;
+  let inputs = document.getElementById("InputSection").children;
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].className.includes("correct")) {
       if (inputs[i].id === optionChosen) {
@@ -48,15 +61,18 @@ _wq.push({
   onReady: function (video) {
     function stop() {
       video.pause();
-      document.querySelectorAll("section")[quizIndex].style.display = "inline";
+      generateQuestionsAndOptions();
+      document.getElementById("section").style.display = "inline";
       document.getElementById("OverlayDiv").style.display = "block";
     }
     function play() {
       if (isQuizComplete[quizIndex] === true) {
         video.play();
-        document.querySelectorAll("section")[quizIndex].style.display = "none";
+        document.getElementById("section").style.display = "none";
         document.getElementById("OverlayDiv").style.display = "none";
         quizIndex++;
+        let divInputs = document.getElementById("InputSection");
+        divInputs.innerHTML = "";
       }
     }
     video.bind("secondchange", function () {
