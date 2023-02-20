355371e3f580d06490a21e581e4259823ea09213
Kaleem Mohammad <62307224+kaleem99@users.noreply.github.com>
2023-02-20 19:02:51 +0200

    Update Interactivator.js

--- Interactivator.js
+++ Interactivator.js
@@ -95,18 +95,19 @@ _wq.push({
     video.bind("play", function () {
       console.log("vide playing.")
       document.getElementById("section").style.display = "none";
+       let timer = setInterval(() => {
+      console.log(video.time(), isQuizComplete[quizIndex]);
+      if (Math.floor(video.time()) === isQuizComplete[quizIndex]) {
+        stop();
+      }
+  }, 1000)
     });
 //     video.bind("secondchange", function () {
 //       if (Math.floor(video.time()) === isQuizComplete[quizIndex]) {
 //         stop();
 //       }
 //     });
-      let timer = setInterval(() => {
-      console.log(video.time(), isQuizComplete[quizIndex]);
-      if (Math.floor(video.time()) === isQuizComplete[quizIndex]) {
-        stop();
-      }
-  }, 1000)
+     
     window.setInterval(play, 1000);
   },
 });
