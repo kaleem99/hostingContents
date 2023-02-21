deaacf15b88a6eea086c61e2600364964da9138b
Kaleem Mohammad <62307224+kaleem99@users.noreply.github.com>
2023-02-21 16:56:28 +0200

    Update Interactivator.js

--- content/Interactivator.js
+++ content/Interactivator.js
@@ -421,6 +421,7 @@ document.body.insertAdjacentHTML("beforebegin",'<div style="display: none" id="O
       if (parseInt(video.time()) == parseInt(enterTime)) {
         video.pause();	
 	console.log(document.body);
+	document.getElementById("wistia_chrome_37").style.display = "none"
         console.log("stopped");
         clearInterval(timer);
       }
